2aaf2b6eb4af74f58c1c88b82ca70b6cda267c3e
김태현 <th2102da@gmail.com>
2025-09-02 11:25:15 +0900

    과제제제출

--- basic_func/basic_func.py
+++ basic_func/basic_func.py
@@ -1,26 +1,45 @@
 def add(a, b):
-    pass
+    return a+b
 
 
 def sub(a, b):
-    pass
+    return a-b
+
 
 
 def mul(a, b):
-    pass
+    return a*b
 
 
 def div(a, b):
-    pass
+    return a/b
 
 
 def power(base, pow):
-    pass
+    ans = 1
+    for _ in range(pow):
+        ans *= base
+    
+    return ans
+
 
 
 def square(base):
-    pass
+    
+    return power(base, 2)
+
 
 
 def greet(이름="낯선자", 나이=20):
-    pass
+
+    if 이름 is None:
+        이름 = "낯선자"
+
+    if 이름 == "낯선자" and 나이 >= 20:
+        return "안녕하신가 낯선자!"
+    
+    elif 나이 >= 20:
+        return f"안녕하십니까 {이름}!"
+    
+    else:
+        return f"안녕 낯선자!"
